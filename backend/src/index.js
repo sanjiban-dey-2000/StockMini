808cbefa64c1a36c9e1a12507f6aaf5b7fa89715
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-07-08 14:26:04 +0530

    Initial setup done

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const dotenv=require('dotenv');
+dotenv.config();
+
+
+const app=express();
+const PORT=process.env.PORT || 5001;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port no ${PORT}`);
+})
